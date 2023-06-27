d6b3ec0ff1583516e362c19b72a1d727fc9bb7cf
suneelmagham <124859694+suneelmagham@users.noreply.github.com>
2023-06-27 12:58:48 +0530

    Add files via upload

--- Java/Activities/Activity1.java
+++ Java/Activities/Activity1.java
@@ -1,24 +1,51 @@
 package activities;
 
 public class Activity1 {
-    public static void main(String[] args){
-        Car obj = new Car();
-//        System.out.println(obj.name);
 
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+
+        //Using Car class method
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
     }
+
 }
+
 class Car {
-    String Color;
-    String Trasmission;
-    int iMake;
-    int iTyres;
-    int iDoors;
+    String color;
+    String transmission;
+    int make;
+    int tyres;
+    int doors;
     //create constructor
-    int  numOfTyres;
+    int numOfTyres;
     int numOfDoors;
-    public Car (){
+
+    public Car() {
         //constructor
-    numOfTyres = 4;
-    numOfDoors = 4;
+        numOfTyres = 4;
+        numOfDoors = 4;
+    }
+
+    //Class Methods
+    public void displayCharacterstics() {
+        System.out.println("Color of the Car: " + color);
+        System.out.println("Make of the Car: " + make);
+        System.out.println("Transmission of the Car: " + transmission);
+        System.out.println("Number of doors on the car: " + doors);
+        System.out.println("Number of tyres on the car: " + tyres);
+    }
+
+    public void accelerate() {
+        System.out.println("Car is moving forward.");
+    }
+
+    public void brake() {
+        System.out.println("Car has stopped.");
     }
 }
